374daa8b1b9f924bd213b5ecc6e40be1b4455770
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-16 15:56:33 -0500

    Estructura inicial

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Alejandro Ariza'),
+      ),
+    );
+  }
+}
