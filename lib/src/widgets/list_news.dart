f2e9ab5e94887d856d29ac5489c2034bb9b67fc3
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-17 16:09:42 -0500

    Diseño del listView para mostrar noticias sin la imagen.

--- lib/src/widgets/list_news.dart
+++ lib/src/widgets/list_news.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/models/news_models.dart';
+import 'package:news/src/theme/darkThemeCustom.dart';
 
 class LitsNews extends StatelessWidget {
   final List<Article> news;
@@ -11,11 +12,102 @@ class LitsNews extends StatelessWidget {
     return ListView.builder(
       itemCount: this.news.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text(
-          this.news[index].title,
-          style: TextStyle(color: Colors.red),
+        return _news(
+          article: this.news[index],
+          index: index,
         );
       },
+    );
+  }
+}
+
+class _news extends StatelessWidget {
+  final Article article;
+  final int index;
+
+  const _news({
+    required this.article,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CardTopBar(
+          article: this.article,
+          index: this.index,
+        ),
+        _CardTitle(
+          article: this.article,
+        ),
+        _CardImage(
+          article: this.article,
+        ),
+      ],
+    );
+  }
+}
+
+class _CardTopBar extends StatelessWidget {
+  final Article article;
+  final int index;
+
+  const _CardTopBar({
+    required this.article,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      margin: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${index + 1}. ',
+            style: TextStyle(color: myThemeDark.accentColor),
+          ),
+          Text(
+            '${article.source!.name}. ',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardTitle extends StatelessWidget {
+  final Article article;
+
+  const _CardTitle({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 15,
+      ),
+      child: Text(
+        article.title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class _CardImage extends StatelessWidget {
+  final Article article;
+
+  const _CardImage({required this.article});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Espacio para la imagen'),
     );
   }
 }
