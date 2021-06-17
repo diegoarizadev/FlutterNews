e95b7dbf489f1644943c55b6bfcb08aea7a2888b
Diego Alejandro Ariza Medina <diego.ariza2@gmail.com>
2021-06-17 16:58:54 -0500

    Implementación CircularProgressIndicator antes de mostrar todas las noticias

--- lib/src/pages/tabsone_page.dart
+++ lib/src/pages/tabsone_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/services/news_services.dart';
+import 'package:news/src/theme/darkThemeCustom.dart';
 import 'package:news/src/widgets/list_news.dart';
 import 'package:provider/provider.dart';
 
@@ -10,8 +11,15 @@ class TabOnePage extends StatelessWidget {
     final newsServices = Provider.of<NewsServices>(
         context); //Obtener los Topheadlines del arbol de Widgets
     //newsServices.headlines
+
     return Scaffold(
-      body: LitsNews(newsServices.headlines),
+      body: (newsServices.headlines.length == 0)
+          ? Center(
+              child: CircularProgressIndicator(
+                color: myThemeDark.accentColor,
+              ),
+            )
+          : LitsNews(newsServices.headlines),
     );
   }
 }
